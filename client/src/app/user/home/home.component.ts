405938555ae38cb9e049385ccd2c8ffe3256512f
Artem <43318829+ArtemPrydorozhko@users.noreply.github.com>
2019-12-15 15:42:20 +0200

    add client post functionality

--- client/src/app/user/home/home.component.ts
+++ client/src/app/user/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { Post, Like } from 'src/app/shared/models/post.model';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +8,20 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('imageLabel', { static: true }) imageLabel: ElementRef;
   public posts: Post[] = [{
     id: 1,
-    dislikes:2,
-    likes:1,
+    dislikes: 2,
+    likes: 1,
     text: 'dfdfd',
     userId: 111,
     mediaUrl: 'https://crowdsourcer.io/assets/images/no-img.png',
     userLike: Like.like,
     userLikeId: 1
-  },{
+  }, {
     id: 1,
-    dislikes:2,
-    likes:1,
+    dislikes: 2,
+    likes: 1,
     text: 'dfdfd',
     userId: 111,
     mediaUrl: null,
@@ -29,11 +30,12 @@ export class HomeComponent implements OnInit {
   }];
   public newPost: FormGroup;
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.newPost = new FormGroup({
-
+      'image': new FormControl(null),
+      'text': new FormControl(null, [Validators.required])
     });
   }
 
@@ -41,4 +43,27 @@ export class HomeComponent implements OnInit {
     this.posts.splice(this.posts.indexOf(post), 1);
   }
 
+  public onCreatePost() {
+    console.log(this.newPost);
+    
+  }
+
+  public uploadFile(event) {
+    let reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.newPost.patchValue({
+          'image': reader.result
+        });
+
+        (<HTMLLabelElement>this.imageLabel.nativeElement).innerText = file.name;
+        this.cd.markForCheck();
+      };
+    }
+  }
 }
